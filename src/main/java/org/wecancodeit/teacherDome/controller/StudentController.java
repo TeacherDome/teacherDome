b36a5b31998ba269ff4b6f57f2edf1b39e25b6f6
Khoi <ktranman@yahoo.com>
2018-11-26 11:53:14 -0500

    controller initialized

---
+++ src/main/java/org/wecancodeit/teacherDome/controller/StudentController.java
@@ -0,0 +1,11 @@
+package org.wecancodeit.teacherDome.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StudentController {
+
+	@Resource
+	StudentRepository studentRepo;
+
+}
