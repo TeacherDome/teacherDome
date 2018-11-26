44b2b8e2e25261e0c22ffe23171f270034530403
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-11-26 11:20:06 -0500

    add student model

---
+++ src/main/java/org/wecancodeit/teacherDome/model/Student.java
@@ -0,0 +1,49 @@
+package org.wecancodeit.teacherDome.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+public class Student {
+
+	private String lastName;
+	private String firstName;
+
+	@GeneratedValue
+	@Id
+	private Long id;
+
+	@Lob
+	private String notes;
+
+	private String schoolId;
+
+	public Student(String lastName, String firstName, String notes, String schoolId) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.notes = notes;
+		this.schoolId = schoolId;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public String getSchoolId() {
+		return schoolId;
+	}
+
+}
+
