e7d649c533c799cb22803e9840f58bac877f76ff
Khoi <ktranman@yahoo.com>
2018-11-26 13:38:03 -0500

    entity annotation added with empty constructor

--- src/test/java/org/wecancodeit/teacherDome/controller/StudentControllerTest.java
+++ src/test/java/org/wecancodeit/teacherDome/controller/StudentControllerTest.java
@@ -1,5 +1,30 @@
 package org.wecancodeit.teacherDome.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.wecancodeit.teacherDome.repo.StudentRepo;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(StudentController.class)
 public class StudentControllerTest {
+
+	@Autowired
+	private MockMvc mock;
+
+	@MockBean
+	StudentRepo repo;
+
+	@Test
+	public void individualStudentPageOK() throws Exception {
+		mock.perform(get("students/1")).andExpect(status().isOk());
+	}
 
 }
