1315d821ba6c0c87328b5b344f31e00886706161
Ingrid Biederman <biederman.14@gmail.com>
2018-11-26 13:42:06 -0500

    Created test repo but not working ATM, updated repo package and class names

--- src/main/java/org/wecancodeit/teacherDome/repo/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package org.wecancodeit.teacherDome.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import org.wecancodeit.teacherDome.model.Student;
-
-public interface StudentRepo extends CrudRepository<Student, Long> {
-
-}
