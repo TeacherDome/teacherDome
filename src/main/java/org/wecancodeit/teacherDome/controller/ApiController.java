1680cc03cb5b68ff62972d0014d070a45da0944b
Riley <rileyjeanpatrick@gmail.com>
2018-11-27 11:51:46 -0500

    api controller now postmapping add student

--- src/main/java/org/wecancodeit/teacherDome/controller/ApiController.java
+++ src/main/java/org/wecancodeit/teacherDome/controller/ApiController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wecancodeit.teacherDome.model.Student;
 import org.wecancodeit.teacherDome.repositories.StudentRepository;
@@ -21,4 +22,11 @@ public class ApiController {
 	public Iterable<Student> getStudent() {
 		return studentRepo.findAll();
 	}
+
+	@PostMapping("/api/students/addStudent")
+	public String addStudent(String studentFirstName, String studentLastName, String studentstudentSchoolIdNumber) {
+		studentRepo.save(new StudentstudentFirstName,studentLastName, studentstudentSchoolIdNumber ));
+		return "index";
+	}
+
 }
