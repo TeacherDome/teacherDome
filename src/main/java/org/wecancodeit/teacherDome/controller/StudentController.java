24b553e8c0c112a5786a1ca4290590e7dbd21ef1
Riley <rileyjeanpatrick@gmail.com>
2018-11-27 10:07:28 -0500

    renaming api controller

--- src/main/java/org/wecancodeit/teacherDome/controller/StudentController.java
+++ src/main/java/org/wecancodeit/teacherDome/controller/StudentController.java
@@ -2,12 +2,10 @@ package org.wecancodeit.teacherDome.controller;
 
 import javax.annotation.Resource;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.wecancodeit.teacherDome.model.Student;
 import org.wecancodeit.teacherDome.repositories.StudentRepository;
 
 @Controller
