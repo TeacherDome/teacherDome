5d931ec64ea070defeaff9ca689e0a143343a910
Riley <rileyjeanpatrick@gmail.com>
2018-12-04 10:45:13 -0500

    fix bugs

--- src/main/java/org/wecancodeit/teacherDome/repositories/StudentRepository.java
+++ src/main/java/org/wecancodeit/teacherDome/repositories/StudentRepository.java
@@ -1,8 +1,10 @@
 package org.wecancodeit.teacherDome.repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 import org.wecancodeit.teacherDome.model.Student;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
-
+	Collection<Student> findByStudentIsRetired(Boolean studentIsRetired);
 }
